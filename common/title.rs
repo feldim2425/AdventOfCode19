02a11d1d841fe39d19462899294e8024e741cd20
feldim2425 <feldim2425@gmail.com>
2019-12-09 21:50:11 +0100

    Add a fancy colored title :D

--- common/title.rs
+++ common/title.rs
@@ -1,4 +1,23 @@
+extern crate colored;
+
+use self::colored::*;
 
 pub fn print_title(day: u8, title: &str){
-    println!("~=~=~= Advent of Code 2019 : Day {} : {} =~=~=~", day, title);
+    for _i in 0..3 {
+        print!("{}", "~".white());
+        print!("{}", "=".red());
+    }
+
+    print!("{}", " Advent of Code ".green());
+    print!("{}", "2019".bright_green().bold());
+    print!("{}", " : ".red());
+    print!("{} {}", "Day".cyan(), format!("{}", day).bright_cyan().bold());
+    print!("{}", " : ".red());
+    print!("{} ", format!("{}", title).purple());
+
+    for _i in 0..3 {
+        print!("{}", "=".red());
+        print!("{}", "~".white());
+    }
+    println!("");
 }
